8f64410c13bcd25f1014adee19fd422159a96ebd
Bryan Haberberger <bryan.j.haberberger@slu.edu>
2023-02-07 16:11:19 -0600

    Don't need this yet

--- app.js
+++ app.js
@@ -8,15 +8,10 @@ var logger = require('morgan')
 
 var app = express()
 
-var indexRouter = require('./routes/index.js')
-
 app.use(logger('dev'))
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 app.use(cookieParser())
-
-app.use('/', indexRouter)
-
 app.use(express.static(path.join(__dirname, '/')))
 
 // catch 404 and forward to error handler
