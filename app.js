f965ff7cca05e0d7326bb96e77c463e8df997b82
Bryan Haberberger <bryan.j.haberberger@slu.edu>
2023-02-07 16:00:12 -0600

    OK this stands up

--- app.js
+++ app.js
@@ -17,6 +17,8 @@ app.use(cookieParser())
 
 app.use('/', indexRouter)
 
+app.use(express.static(path.join(__dirname, '/')))
+
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404))
