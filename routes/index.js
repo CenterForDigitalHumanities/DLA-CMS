06537f5bdde0762dfbf4801751f114568e15641a
cubap <cubap@slu.edu>
2023-02-13 16:48:02 -0600

    resolved

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,13 @@
 const express = require('express')
 const path = require('path')
 
-const manageRoutes = require('./manage')
-
 const indexRouter = express.Router()
+
+const manageRoutes = require('./manage')
 indexRouter.use('/manage', manageRoutes)
+
+const reportsRouter = require('./reports')
+indexRouter.use('/status', reportsRouter)
 
 indexRouter.get('/',(req,res)=>res.redirect('/index.html'))
 
